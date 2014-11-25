56f1677ed196e78e3894b95d193ca89a430f65db
ebith <ebith.h@gmail.com>
2014-11-25 11:36:33 +0900

    動くようにした

--- greasemonkey/nicovideo-watch-page-fix.user.js
+++ greasemonkey/nicovideo-watch-page-fix.user.js
@@ -2,7 +2,7 @@
 /*
 // ==UserScript==
 // @name nicovideo watch page fix
-// @version 1.1
+// @version 1.2
 // @author ebith
 // @include http://www.nicovideo.jp/watch/*
 // @grant none
@@ -11,28 +11,52 @@ ref: https://greasyfork.org/scripts/452-shinjukuwatch/code
  */
 
 (function() {
-  var main;
+  var WIM, main, onReset;
 
-  WatchApp.ns.model.player.NicoPlayerConnector.onTagDataReceived = function() {};
+  WIM = require('watchapp/model/WatchInfoModel').getInstance();
 
-  WatchApp.ns.model.player.NicoPlayerConnector.getCommentNicoruCount = function() {
-    return 0;
-  };
-
-  (main = function() {
-    var controller;
-    if (!(controller = WatchApp.ns.init.VideoExplorerInitializer.videoExplorerController)) {
+  if (WIM.initialized) {
+    main();
+  } else {
+    onReset = function() {
+      WIM.removeEventListener('reset', onReset);
       return setTimeout((function() {
         return main();
-      }), 1000);
-    } else {
-      controller.showMylist = $.proxy(function(id) {
-        return location.href = "/mylist/" + id;
-      }, controller);
-      return controller.searchVideo = $.proxy(function(word, type) {
-        return location.href = (type === 'tag' ? 'tag' : 'search') + ("/" + (encodeURIComponent(word)));
-      }, controller);
-    }
+      }), 100);
+    };
+    WIM.addEventListener('reset', onReset);
+  }
+
+  (main = function() {
+    var css, style;
+    css = '#videoHeaderTagList > .locked:before { content: "🔒"; color: #b5a642;}';
+    style = document.createElement('style');
+    style.setAttribute('type', 'text/css');
+    style.textContent = css;
+    document.body.appendChild(style);
+    WatchApp.ns.api.TagAPILoader.prototype.getTags(WatchJsApi.video.getVideoID(), function(status, data) {
+      var tagList;
+      tagList = document.getElementById("videoHeaderTagList").getElementsByClassName("videoHeaderTag");
+      return data.tags.forEach(function(tag) {
+        if (tag.owner_lock === 1) {
+          return Array.forEach(tagList, function(elem) {
+            if (elem.firstChild.textContent === tag.tag) {
+              return elem.classList.add('locked');
+            }
+          });
+        }
+      });
+    });
+    WatchApp.ns.model.player.NicoPlayerConnector.onTagDataReceived = function() {};
+    WatchApp.ns.model.player.NicoPlayerConnector.getCommentNicoruCount = function() {
+      return 0;
+    };
+    controller.showMylist = $.proxy(function(id) {
+      return location.href = "/mylist/" + id;
+    }, controller);
+    return controller.searchVideo = $.proxy(function(word, type) {
+      return location.href = (type === 'tag' ? 'tag' : 'search') + ("/" + (encodeURIComponent(word)));
+    }, controller);
   })();
 
 }).call(this);
--- greasemonkey/src/nicovideo-watch-page-fix.user.coffee
+++ greasemonkey/src/nicovideo-watch-page-fix.user.coffee
@@ -1,7 +1,7 @@
 ###
 // ==UserScript==
 // @name nicovideo watch page fix
-// @version 1.1
+// @version 1.2
 // @author ebith
 // @include http://www.nicovideo.jp/watch/*
 // @grant none
@@ -9,20 +9,40 @@
 ref: https://greasyfork.org/scripts/452-shinjukuwatch/code
 ###
 
-# タグを自動更新しない
-WatchApp.ns.model.player.NicoPlayerConnector.onTagDataReceived = ->
+WIM = do require('watchapp/model/WatchInfoModel').getInstance
+if WIM.initialized
+  do main
+else
+  onReset = ->
+    WIM.removeEventListener 'reset', onReset
+    setTimeout (-> do main), 100
+  WIM.addEventListener 'reset', onReset
 
-# ニコる数を取得しない
-WatchApp.ns.model.player.NicoPlayerConnector.getCommentNicoruCount = -> 0
-
-# 普通にページ遷移する
 do main = ->
-  if not controller = WatchApp.ns.init.VideoExplorerInitializer.videoExplorerController
-    setTimeout (-> do main), 1000
-  else
-    controller.showMylist = $.proxy (id) ->
-      location.href = "/mylist/#{id}"
-    , controller
-    controller.searchVideo = $.proxy (word, type) ->
-      location.href = (if type is 'tag' then 'tag' else 'search') + "/#{encodeURIComponent word}"
-    , controller
+  # タグロックを可視化する
+  css = '#videoHeaderTagList > .locked:before { content: "🔒"; color: #b5a642;}'
+  style = document.createElement 'style'
+  style.setAttribute 'type', 'text/css'
+  style.textContent = css
+  document.body.appendChild style
+
+  WatchApp.ns.api.TagAPILoader::getTags WatchJsApi.video.getVideoID(), (status, data) ->
+    tagList = document.getElementById("videoHeaderTagList").getElementsByClassName("videoHeaderTag")
+    data.tags.forEach (tag) ->
+      if tag.owner_lock is 1
+        Array.forEach tagList, (elem) ->
+          elem.classList.add 'locked'  if elem.firstChild.textContent is tag.tag
+
+  # タグを自動更新しない
+  WatchApp.ns.model.player.NicoPlayerConnector.onTagDataReceived = ->
+
+  # ニコる数を取得しない
+  WatchApp.ns.model.player.NicoPlayerConnector.getCommentNicoruCount = -> 0
+
+  # 普通にページ遷移する
+  controller.showMylist = $.proxy (id) ->
+    location.href = "/mylist/#{id}"
+  , controller
+  controller.searchVideo = $.proxy (word, type) ->
+    location.href = (if type is 'tag' then 'tag' else 'search') + "/#{encodeURIComponent word}"
+  , controller
